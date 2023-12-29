e3fa4019bfd19735b3745c8772d5be11404f5f68
GGB <rlqja1018@gmail.com>
2023-12-29 16:51:39 +0900

    틱택토 게임
    가로/세로 승리 조건 검사 완료
    
    커밋 전 진행상황 ->
    P1,P2 돌아가며 X,O 문자로 본인이 선택한 지점 선택 및 저장 까지 작성 되어 있엇음.

--- Study/TicTT.cs
+++ Study/TicTT.cs
@@ -16,23 +16,29 @@ public class TicTT
                 board[i, j] = char.Parse(n++.ToString());
             }
         }
-        
+
         Console.WriteLine("입력 방식 : 1~9 를 선택");
         Console.WriteLine("P1 : X, P2: O");
         printBoard(board);
 
-        int turn = 0;
+        int playeNum = 0;
+        int turn = 0; 
         string str;
         while (true)
         {
-            turn = turn % 2 == 0 ? 1 : 2;    // 나머지가 0이면 P1 1이면 P2
-            Console.Write($"P{turn} : ");
+            // 나머지가 0이면 P1 1이면 P2  1->2->1 반복
+            playeNum = turn % 2 == 0 ? 1 : 2;
+            Console.Write($"P{playeNum} : ");
             str = Console.ReadLine();
 
             board = insertBoard(board, turn, str);
             printBoard(board);
 
             // 승리/무승부 조건 검사
+            if (turn > 3) // 최소 5턴부터 승리자가 나올 수 있음
+                GameOver(board);
+            //ㅡㅡㅡㅡㅡ조건검사 끝
+            turn++;
         }
     }
 
@@ -65,6 +71,82 @@ public class TicTT
         }
 
         return board;
+    }
+
+    static void GameOver(char[,] board)
+    {
+        char[] checkArr = new char[board.GetLength(1)];
+
+        // 가로 검사
+        bool pass = false;
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (board[i, j] != 'X' && board[i, j] != 'O')
+                {
+                    pass = true;
+                    Array.Clear(checkArr, 0, checkArr.Length);
+                    break;
+                }
+
+                checkArr[j] = board[i, j];
+            }
+            if (!pass)
+                Check(checkArr);
+        }
+
+        // 세로 검사
+        pass = false;
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (board[j, i] != 'X' && board[j, i] != 'O')
+                {
+                    pass = true;
+                    Array.Clear(checkArr, 0, checkArr.Length);
+                    break;
+                }
+
+                checkArr[j] = board[j, i];
+            }
+            if (!pass)
+                Check(checkArr);
+        }
+
+        // 대각선 검사
+        //checkCnt = 0; pass = false;
+        //for (int i = 0; i < board.GetLength(0); i++)
+        //{
+        //    if (!(board[i, i] == 'O') && !(board[i, i] == 'X'))
+        //        pass = true; break;
+
+        //    checkArr[i] = board[i, i];
+        //}
+
+
+    }
+
+    static void Check(char[] checkArr)
+    {
+        int checkCnt = 0;
+        for (int i = 0; i < checkArr.Length; i++)
+        {
+            if (checkArr[i] == 'X')
+                checkCnt++;
+        }
+
+        if (checkCnt == checkArr.Length)
+        {
+            //P1 승리
+            Console.WriteLine("P1 Win!!!!!");
+        }
+        else if (checkCnt == 0)
+        {
+            //P2 승리
+            Console.WriteLine("P2 Win!!!!!");
+        }
     }
 
     static void Main(string[] args)
